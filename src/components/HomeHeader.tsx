ffdac7d3bdb6fecc2c085b06dae5fdbf4808b46c
Julio Machado <juliocarlos00@hotmail.com>
2024-09-22 20:07:08 +0100

    add/ home header

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,26 @@
+import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
+import { LogOut } from "lucide-react-native";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: "https://github.com/juliomchado.png" }}
+        alt="User Image"
+        w="$16"
+        h="$16"
+      />
+      <VStack flex={1}>
+        <Text color="$gray100" fontSize="$sm">
+          Olá,
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Julio Machado
+        </Heading>
+      </VStack>
+
+      <Icon as={LogOut} color="$gray200" size="xl" />
+    </HStack>
+  );
+}
