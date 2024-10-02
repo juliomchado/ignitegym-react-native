b54f1e4ee0d08d22be1970ca09bb80f99cd8f71d
Julio Machado <juliocarlos00@hotmail.com>
2024-10-02 03:30:56 +0100

    add/ jwt validation

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,18 +1,24 @@
-import { UserDTO } from "@dtos/UserDTO";
-import { api } from "@services/api";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+import {
+  storageAuthTokenSave,
+  storageAuthTokenGet,
+  storageAuthTokenRemove,
+} from "@storage/storageAuthToken";
 import {
   storageUserGet,
   storageUserRemove,
   storageUserSave,
 } from "@storage/storageUser";
-import { UserPlus } from "lucide-react-native";
-import { createContext, ReactNode, useEffect, useState } from "react";
+
+import { api } from "@services/api";
+import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
-  isLoadingUserStorageData: boolean;
   signOut: () => Promise<void>;
+  isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -28,20 +34,36 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
-  async function signIn(email: string, password: string) {
-    try {
-      const { data } = await api.post("/sessions", {
-        email,
-        password,
-      });
+  async function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-      if (data.user) {
-        setUser(data.user);
-        storageUserSave(data.user);
-      }
-      console.log(data);
+    setUser(userData);
+  }
+
+  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
     } catch (error) {
       throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post("/sessions", { email, password });
+
+      if (data.user && data.token) {
+        await storageUserAndTokenSave(data.user, data.token);
+        userAndTokenUpdate(data.user, data.token);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -49,7 +71,10 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       setIsLoadingUserStorageData(true);
       setUser({} as UserDTO);
+      api.defaults.headers.common.Authorization = "";
+
       await storageUserRemove();
+      await storageAuthTokenRemove();
     } catch (error) {
       throw error;
     } finally {
@@ -59,11 +84,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   async function loadUserData() {
     try {
-      const userLogged = await storageUserGet();
+      setIsLoadingUserStorageData(true);
 
-      if (userLogged) {
-        setUser(userLogged);
-        setIsLoadingUserStorageData(false);
+      const userLogged = await storageUserGet();
+      const token = await storageAuthTokenGet();
+
+      if (token && userLogged) {
+        userAndTokenUpdate(userLogged, token);
       }
     } catch (error) {
       throw error;
@@ -78,7 +105,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   return (
     <AuthContext.Provider
-      value={{ user, signIn, signOut, isLoadingUserStorageData }}
+      value={{
+        user,
+        signIn,
+        signOut,
+        isLoadingUserStorageData,
+      }}
     >
       {children}
     </AuthContext.Provider>
